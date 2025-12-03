6a6fe220f055ab8219a548319c1faca8273732d7
Utkarsh <onlyutkarshjoshilkar@gmail.com>
2025-12-03 08:38:04 +0530

    Auth request class fro the request object

--- authify/src/main/java/in/utkarsh/authify/io/AuthRequest.java
+++ authify/src/main/java/in/utkarsh/authify/io/AuthRequest.java
@@ -1,8 +1,14 @@
 package in.utkarsh.authify.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthRequest {
 
     private String email;
